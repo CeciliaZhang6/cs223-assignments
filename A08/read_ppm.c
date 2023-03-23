998811dd42ca99eab94cfec0ed7a962da97b791f
Cecilia Zhang <czhang1@goldengate.cs.brynmawr.edu>
2023-03-22 20:08:38 -0400

    A08 complete

--- A08/read_ppm.c
+++ A08/read_ppm.c
@@ -1,14 +1,56 @@
+/*----------------------------------------------
+ * Author: Cecilia Zhang
+ * Date: 02/21/2023
+ * Description: A function that reads a given ppm file, stores width, height, 
+ *   and binary data of each pixel's RGB value (in an array of pixel struct). 
+ *   It updates the values of width and height, and returns a pointer to the 
+ *   pixel array it read in.
+ ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
 
+/**
+* See description in file header. 
+* @param filename the name of input ppm file. 
+* @param w int pointer to width
+* @param h int pointer to height
+* @return 1D array of pixels it read in. NULL if file can not be read. 
+*/
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *infile;
+  infile = fopen(filename, "rb");
+  if (infile == NULL) {
+    printf("Error! Can't open file: %s\n", filename);
+    return NULL;
+  }
+  char buffer[1024];
+  int width = 0;
+  int height = 0;
+  //skip title lines
+  fgets(buffer, 1024, infile);
+  fgets(buffer, 1024, infile);
+  while (buffer[0] == '#') {
+    fgets(buffer, 1024, infile);
+  }
+  
+  //read and store the width & height
+  sscanf(buffer, "%d%*c %d%*c", &width, &height);
+  //fscanf(infile, "%d%*c", &height);
+  //skip line with "255"
+  fgets(buffer, 1024, infile);
 
-  return NULL;
+  struct ppm_pixel* pxl = malloc(sizeof(struct ppm_pixel) * width * height);
+  fread(pxl, sizeof(struct ppm_pixel), (width * height), infile);
+
+  fclose(infile);
+  //return
+  *w = width;
+  *h = height;
+  return pxl;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-
   return NULL;
 }
--- A08/single_mandelbrot.c
+++ A08/single_mandelbrot.c
@@ -5,6 +5,7 @@
 #include <sys/time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+#include <string.h>
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -29,10 +30,67 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
+  // program timer
+  double timer;
+  struct timeval tStart, tEnd;
+  // start timer
+  gettimeofday(&tStart, NULL);
+  //set seed
   srand(time(0));
 
-  // compute image
+  struct ppm_pixel* pixels = malloc(sizeof(struct ppm_pixel) * size * size);
+  struct ppm_pixel* palette = malloc(sizeof(struct ppm_pixel) * maxIterations); 
+  float xfrac, yfrac, x0, y0, x, y, xtemp;
+  int iter;
+  float imageSize = size;
 
+  //generate palette
+  for (int i = 0; i < maxIterations; i++) {
+    palette[i].red = rand() % 255;
+    palette[i].green = rand() % 255;
+    palette[i].blue = rand() % 255;
+  }
+
+  // compute image
+  for (int i = 0; i < size; i++) {  // i = rows
+    for (int j = 0; j < size; j++) {  // j = cols
+      xfrac = i / imageSize; 
+      yfrac = j / imageSize; 
+      x0 = xmin + xfrac * (xmax - xmin);
+      y0 = ymin + yfrac * (ymax - ymin);
+
+      x = 0;
+      y = 0;
+      iter = 0;
+
+      while (iter < maxIterations && (x*x + y*y) < 2*2) {
+        xtemp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtemp;
+        iter++;
+      }
+      //put colors inside each pixel
+      if (iter < maxIterations) {
+        pixels[j * size + i] = palette[iter];
+      } else {
+        pixels[j * size + i].red = 0;
+        pixels[j * size + i].green = 0;
+        pixels[j * size + i].blue = 0;
+      }
+    }
+  }
+  gettimeofday(&tEnd, NULL);
+
+  // compute time
+  timer = tEnd.tv_sec - tStart.tv_sec + (tEnd.tv_usec - tStart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size, timer);
+
+  // write output file
+  char filename[1024];
+  snprintf(filename, 1024, "mandelbrot-%d-%lu.ppm", size, time(0)); 
+  write_ppm(filename, pixels, size, size);
+  printf("Writing file: %s\n", filename);
+
+  free(pixels);
+  free(palette); 
 }
