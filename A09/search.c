caa18502f755eab5cf8cb9a91ca9f3adab801778
Cecilia Zhang <czhang1@goldengate.cs.brynmawr.edu>
2023-04-05 17:57:53 -0400

    A09 Part 1 Complete

--- A09/search.c
+++ A09/search.c
@@ -1,5 +1,48 @@
+/*----------------------------------------------
+ * Author: Cecilia Zhang
+ * Date: 04/04/2023
+ * Description: Reads in a binary file and stores data into an int array.
+ *    Utilized multiple threads to search simultaneously for a given int. 
+ ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+struct threadArgs {
+  int startIdx;
+  int endIdx;
+  int* nums;
+  int targetNum;
+  int id;
+  int found;
+};
+
+/**
+* Thread function. Searches for the target number and prints out its index if exists. 
+* @param inputData all data required to compute. See struct above for more info. 
+*/
+void *searchNum(void* inputData) {
+  // cast input to struct
+  struct threadArgs * myArgs = (struct threadArgs *) inputData;
+
+  // extract variables
+  int threadId = myArgs->id;
+  int* nums = myArgs->nums;
+  int startIndex = myArgs->startIdx;
+  int endIndex = myArgs->endIdx;
+  int targetNum = myArgs->targetNum;
+
+  // search
+  for (int i = startIndex; i < endIndex; i++) {
+    if (nums[i] == targetNum){
+      printf("Thread %d found %d at index %d\n", threadId, targetNum, i);
+      myArgs->found = 1;
+    }
+  }
+  return NULL;
+}
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -7,5 +50,59 @@ int main(int argc, char** argv) {
     exit(0);
   }
 
+  int size = 0;
+  FILE* fp = fopen("data.bin", "rb");
+
+  // read array size
+  fread(&size, sizeof(int), 1, fp);
+  int *nums = NULL;
+  nums = malloc(sizeof(int) * size);
+  fread(nums, sizeof(int), size, fp);
+
+  // the number we will look for
+  int targetNum = 0;
+  printf("Enter a value to search: ");
+  scanf("%d", &targetNum);
+
+  // set up threads
+  int numThreads = atoi(argv[1]);
+  pthread_t *threads = malloc(sizeof(pthread_t) * numThreads);
+  struct threadArgs *tArgs = malloc(sizeof(struct threadArgs) * numThreads);
+
+  // fill in thread args
+  for (int i = 0; i < numThreads; i++) {
+    tArgs[i].startIdx = i * (size/numThreads);
+    tArgs[i].endIdx = tArgs[i].startIdx + (size/numThreads);
+    tArgs[i].nums = nums;
+    tArgs[i].id = i + 1; // thread number starts from 1
+    tArgs[i].targetNum = targetNum;
+    tArgs[i].found = 0;
+  }
+
+  // create threads
+  for (int i = 0; i < numThreads; i++) {
+    pthread_create(&threads[i], NULL, searchNum, &tArgs[i]);
+  }
+
+  // wait for thread join
+  for (int i = 0; i < numThreads; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  // check if not found in all threads
+  int notFound = 1;
+  for (int i = 0; i < numThreads; i++) {
+    if (tArgs[i].found == 1) {
+      notFound = 0;
+    }
+  }
+  if (notFound == 1) {
+    printf("Not found!\n");
+  }
+
+  fclose(fp);
+  free(nums);
+  free(threads);
+  free(tArgs);
   return 0;
 }
--- A09/write_ppm.c
+++ A09/write_ppm.c
@@ -1,9 +1,45 @@
+/*----------------------------------------------
+ * Author: Cecilia Zhang
+ * Date: 02/21/2023
+ * Description: A function that writes title lines, width, height, max color value, 
+ *   and all binary data (from a given pixel array) into a given file. 
+ ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+/**
+* See description in file header. 
+* @param filename the name of output file it will write to. 
+* @param pixels pointer to a 1D array of pixel to save (detination).
+* @param w width.
+* @param h height.
+* No return.
+*/
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+  FILE *outfile = fopen(filename, "wb");
+  if (outfile == NULL) {
+    printf("File not opened. \n");
+    exit(0);
+  }
+
+  //write title lines
+  fprintf(outfile, "%s\n", "P6");
+  fprintf(outfile, "%s\n", "# Created by GIMP version 2.10.24 PNM plug-in");
+
+  //write width and height
+  fprintf(outfile, "%d %d\n", w, h);
+
+  //write max color value
+  fprintf(outfile, "%d\n", 255);
+
+  //write bianry data
+  fwrite(pixels, sizeof(struct ppm_pixel), (w*h), outfile);
+
+  fclose(outfile);
 }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+
 }
